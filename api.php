2268c85a5ed745f9f6d7e0a07140149feda69351
Yohei Murayama <muracchi@gmail.com>
2011-07-12 00:32:37 +0900

    use Zend Framework

--- api.php
+++ api.php
@@ -1,62 +1,80 @@
 <?php
-require_once 'Phlickr/Api.php';
-require_once 'Phlickr/Request.php';
-require_once 'Phlickr/PhotoList.php';
-require_once 'Phlickr/PhotoListIterator.php';
-require_once 'Phlickr/Favorites.php';
+require_once 'Zend/Loader.php';
+Zend_Loader::loadClass('Zend_Service_Flickr');
 
 define('FLICKR_API_KEY',    '728ccdde61aed3205c14655d0c47cb85');
 define('FLICKR_API_SECRET', '8cdb9a7a50b1a52f');
 define('DEFAULT_TAG_NAME',  'kosenconf');
 
-$api = new Phlickr_Api(FLICKR_API_KEY, FLICKR_API_SECRET);
 $tag = DEFAULT_TAG_NAME;
-$page = 1;
 if (isset($_REQUEST['tag']) ) {
     $tag = $_REQUEST['tag'];
 }
+$page = 1;
 if (isset($_REQUEST['page']) ) {
     $page = (int)$_REQUEST['page'];
 }
 
-$request = $api->createRequest(
-    'flickr.photos.search',
+$flickr = new Zend_Service_Flickr(FLICKR_API_KEY);
+$results = $flickr->tagSearch(
+    $tag,
     array(
-        'tags' => $tag,
+        'per_page' => 20,
         'sort' => 'date-taken-desc',
         'safe_search' => '1',
     )
 );
 
-$photolist = new Phlickr_PhotoList($request, 30);
-if ($page > 1) $photolist->setPage($page);
-$photos = $photolist->getPhotos();
-
 $photo_url_array = array();
-foreach ($photos as $photo) {
-    $weight = getWeight($api, $photo);
-    $thumb_size =  ($weight == 1.0) ? Phlickr_Photo::SIZE_500PX : 
-                  (($weight == 0.5) ? Phlickr_Photo::SIZE_240PX :
-                                      Phlickr_Photo::SIZE_75PX );
-    $photo_info = array('thumb' => $photo->buildImgUrl($thumb_size),
-                        'url' => $photo->buildUrl(),
-                        'weight' => $weight);
+foreach ($results as $result) {
+    $weight = getWeight($flickr, $result->id);
+	$thumb = null;
+	if ($weight == 1.0) {
+	    $thumb = $result->Medium;
+	} else if ($weight == 0.5) {
+	    $thumb = $result->Small;
+	} else {
+	    $thumb = $result->Square;
+	}
+	$url = Zend_Service_Flickr::URI_BASE . '/photos/' . $result->owner . '/' . $result->id;
+    $photo_info = array(
+	    'thumb'  => $thumb->uri,
+		'height' => $thumb->height,
+		'width'  => $thumb->width,
+        'url'    => $url,
+        'weight' => $weight
+    );
+    //var_dump($photo_info);
+
     $photo_url_array[] = $photo_info;
 }
 
 header('Content-type: application/json');
 print json_encode($photo_url_array);
 
-function getWeight($api, $photo) {
-    $request = $api->createRequest(
-        'flickr.photos.getFavorites',
-        array(
-            'photo_id' => $photo->getId(),
-        )
-    );
+function getWeight($flickr, $id) {
+    static $method = 'flickr.photos.getFavorites';
 
-    $favorites = new Phlickr_Favorites($request);
-    $count = $favorites->getTotal();
+    if (empty($id)) {
+        /**
+         * @see Zend_Service_Exception
+         */
+        require_once 'Zend/Service/Exception.php';
+        throw new Zend_Service_Exception('You must supply a photo ID');
+    }
+
+    $options = array('api_key' => $flickr->apiKey, 'method' => $method, 'photo_id' => $id);
+
+    $restClient = $flickr->getRestClient();
+    $restClient->getHttpClient()->resetParameters();
+    $response = $restClient->restGet('/services/rest/', $options);
+	//var_dump($response->getBody());
+
+    $dom = new DOMDocument();
+    $dom->loadXML($response->getBody());
+	//todo check
+    $xpath = new DOMXPath($dom);
+    $count = (int) $xpath->query('//photo')->item(0)->getAttribute('total');
     return   ($count >= 10) ? 1.0 :
             (($count >   0) ? 0.5 : 0.0 );
 }
